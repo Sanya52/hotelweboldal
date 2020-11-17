85ca2bce3132a2d5744a1205dc362776f8253574
Sanya52 <70943490+Sanya52@users.noreply.github.com>
2020-11-17 13:28:34 +0100

    Add files via upload
    A javascript még nem fut le

---
+++ hotelweboldal/main.js
@@ -0,0 +1,12 @@
+let szoba=document.getElementsByName=("sur");
+let ejj=document.getElementById=("ej").value;
+let fo=document.getElementById=("fo").value;
+
+function tart(){
+    ejj++;
+    alert(ej);
+}
+function lat(){
+    fo++;
+    alert(fo);
+}
